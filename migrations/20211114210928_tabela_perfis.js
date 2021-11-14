3cdc89e52bd16a574f3d56e7cfc0ceea85389956
Natanael Araujo <natanael@hotmail.co.jp>
2021-11-14 19:48:02 -0300

    Criando e implementando as configuracoes iniciais das Migrations

--- migrations/20211114210928_tabela_perfis.js
+++ migrations/20211114210928_tabela_perfis.js
@@ -1,9 +1,17 @@
-// Gerado por: npx knex migrate:make tabela_perfis
-
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('perfis', table => {
+        table.increments('id').primary();
+        table.string('nivel').notNull().unique();
+        table.string('rotulo').notNull();
+    }).then(() => {
+        return knex('perfis').insert([
+            { nivel: 'comum', rotulo: 'comum' },
+            { nivel: 'admin', rotulo: 'administrador' },
+            { nivel: 'master', rotulo: 'Master' }
+        ])
+    })
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTable('perfis');
 };
--- migrations/20211114211304_tabela_usuarios_perfis.js
+++ migrations/20211114211304_tabela_usuarios_perfis.js
@@ -1,9 +1,13 @@
-// Gerado por: npx knex migrate:make tabela_usuarios_perfis
-
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('usuarios_perfis', table => {
+        table.integer('usuario_id').unsigned();
+        table.integer('perfil_id').unsigned();
+        table.foreign('usuario_id').references('usuarios.id');
+        table.foreign('perfil_id').references('perfis.id');
+        table.primary(['usuario_id', 'perfil_id'])
+    })
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTable('usuarios_perfis');
 };
