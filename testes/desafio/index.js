9d9266d00293b5bf6f2e0dbcfd73c4bc610dacef
Natanael Araujo <natanael@hotmail.co.jp>
2021-11-15 04:50:30 -0300

    Projeto inicial do desafio: banco de dados & GraphQL

---
+++ testes/desafio/index.js
@@ -0,0 +1,28 @@
+const { join } = require('path')
+const { loadSchemaSync } = require('@graphql-tools/load')
+const { addResolversToSchema } = require('@graphql-tools/schema')
+const { UrlLoader } = require('@graphql-tools/url-loader')
+const { GraphQLFileLoader } = require('@graphql-tools/graphql-file-loader')
+const { JsonFileLoader } = require('@graphql-tools/json-file-loader')
+
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
+
+const schema = loadSchemaSync(join(__dirname, './schema/index.graphql'), {
+    loaders: [new GraphQLFileLoader()]
+})
+
+const resolvers = require('./resolvers/index.js')
+
+const schemaWithResolvers = addResolversToSchema({
+    schema,
+    resolvers
+})
+
+const app = express();
+app.use('/graphql', graphqlHTTP({
+  schema: schemaWithResolvers,
+  graphiql: true,
+}));
+app.listen(4000, () => console.log('Executando em: http://localhost:4000/graphql'));
---
+++ testes/desafio/resolvers/Query/perfil.js
@@ -0,0 +1,10 @@
+const db = require('../../config/db')
+
+module.exports = {
+    perfis() {
+        // implementar
+    },
+    async perfil(_, { filtro }) {
+        // implementar
+    }
+}
