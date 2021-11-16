89c9e79c03b70b0d1155e24a5b7275f114f5031b
Natanael Araujo <natanael@hotmail.co.jp>
2021-11-16 00:45:45 -0300

    Implementando o Resolver da Mutation perfil: novoPerfil

--- testes/desafio/resolvers/Mutation/perfil.js
+++ testes/desafio/resolvers/Mutation/perfil.js
@@ -1,8 +1,18 @@
 const db = require('../../config/db')
 
 module.exports = {
+    // dados é um objeto que contém os dados do usuário
     async novoPerfil(_, { dados }) {
-        // implementar
+        try {
+            // Recebe o id de dentro do array que é retornado pelo método insert
+            const [ id ] = await db('perfis')
+            .insert(dados)
+            // Retorna os dados a partir do id, obs.: se usar desestruturação, talvez seja necessário usar {  }
+            return db('perfis')
+                .where( { id } ).first();
+        } catch (e) {
+            throw new Error(e.sqlMessage)
+        }
     },
     async excluirPerfil(_, { filtro }) {
         // implementar
